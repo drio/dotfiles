e8b6e205335cfe498ccf3456168b068fedafd12e
David Rio Deiros <driodeiros@gmail.com>
2022-12-30 08:49:20 -0500

    svelte lsp + synergy keys

--- .config/nvim/lua/drio/plugins/lsp/mason.lua
+++ .config/nvim/lua/drio/plugins/lsp/mason.lua
@@ -30,6 +30,7 @@ mason_lspconfig.setup({
 		"gopls",
 		"terraformls",
 		"yamlls",
+		"svelte-language-server",
 	},
 	-- auto-install configured servers (with lspconfig)
 	automatic_installation = true, -- not the same as ensure_installed
